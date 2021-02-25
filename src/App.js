cbfabe147a02f702e5d0b7b547e3777315eab028
Barkelgne Gatahun <barklegne.eshetu@gmail.com>
2021-02-25 06:16:18 +0300

    Boilerplate for CRA and styled component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import styled from 'styled-components';
+import logo from './assets/images/logo.svg';
+
 function App() {
 	return (
-		<div>
-			<h2>Impact team landing page</h2>
-		</div>
+		<Wrapper>
+			<p>Landing page</p>
+			{/* <h2 className='heading bg'>Impact team landing page..</h2> */}
+			{/* <img src={logo} alt='logo' /> */}
+		</Wrapper>
 	);
 }
 
+const Wrapper = styled.div`
+	.heading {
+		font-size: var(--default-font-size);
+		color: var(--color-primary);
+		margin-top: 2rem;
+	}
+`;
 export default App;
